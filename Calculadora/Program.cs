a085ab91920904c5a2d0f9b0ce41f16d2a60f829
Morella Cantos <cantosmorella@gmail.com>
2025-06-21 18:57:03 -0300

    Ejercicio 1

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EspacioCalculadora;
+Console.WriteLine("""Hello, World!""");
+int operacion,num = 0;
+string? s1,s2;
+Calculadora calcu = new Calculadora();
+do
+{
+    Console.WriteLine($"Ingrese la operacion:\n1.Suma\n2.Resta\n3.Multiplicacion\n4.Division\n5.Limpiar\nResultado {calcu.Resultado}");
+    s1 = Console.ReadLine();
+    bool resultado = int.TryParse(s1, out operacion);
+
+    switch (operacion)
+    {
+        case 1:
+            Console.WriteLine("Ingrese el numero a sumar");
+            s2 = Console.ReadLine();
+            resultado = int.TryParse(s2, out num);
+            calcu.Sumar(num);
+            break;
+    }
+
+} while (operacion < 5);
